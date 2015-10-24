cd16cfbc5224b25f0016ac4ab2ead82ee84a3c14
Rostislav Chekan <r.chekan@gmail.com>
2015-10-24 09:54:39 +0300

    Kapt doesn't produce error that MainActivity is final

---
+++ app/src/main/java/com/example/astra/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.astra.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EActivity
+
+@EActivity
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    @AfterViews
+    internal fun afterViews(){
+
+    }
+}
